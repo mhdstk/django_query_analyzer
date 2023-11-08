53120593212c9b39a122a873cdd89d3b6e7ac575
Shaheen <muhammedshaheen.tkb@gmail.com>
2023-11-08 18:50:18 +0530

    listing reversed

--- django_query_analyzer/views.py
+++ django_query_analyzer/views.py
@@ -3,5 +3,5 @@ from .models import QueryAnalyzer
 
 
 def query_analyzer_list(request):
-    query_analyzers = QueryAnalyzer.objects.all()
+    query_analyzers = QueryAnalyzer.objects.all().order_by('-created_at')
     return render(request, 'django_query_analyzer/django_query_analyzer.html', {'query_analyzers': query_analyzers})
